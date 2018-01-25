c5c6d371577a42c333d8f1eb7d5b98ceb13a0989
Kimberly Zick (rummik) <k@9k1.us>
2018-01-24 20:18:05 -0500

    Add Discord and Mastodon bitlbee plugin packages

--- cfgs/bitlbee.nix
+++ cfgs/bitlbee.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 
+with import <nixpkgs> {};
 {
   services.bitlbee = {
     enable = true;
@@ -7,6 +8,9 @@
     plugins = with pkgs; [
       bitlbee-facebook
       bitlbee-steam
+
+      (callPackage ../pkgs/bitlbee-mastodon/default.nix { })
+      (callPackage ../pkgs/bitlbee-discord/default.nix { })
     ];
 
     libpurple_plugins = with pkgs; [
