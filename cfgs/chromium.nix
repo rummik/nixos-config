b97f31d6b5434d08fe207f1db7997f360ae3bb67
Kimberly Zick (rummik) <k@9k1.us>
2018-02-02 13:46:28 -0500

    Add some default Chrome extensions

--- cfgs/chromium.nix
+++ cfgs/chromium.nix
@@ -8,4 +8,11 @@
   environment.variables = {
     BROWSER = pkgs.lib.mkOverride 0 "chromium";
   };
+
+  programs.chromium.extensions = [
+    "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+    "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
+    "gcbommkclmclpchllfjekcdonpmejbdp" # HTTPS Everywhere
+    "klbibkeccnjlkjkiokjodocebajanakg" # The Great Suspender
+  ];
 }
