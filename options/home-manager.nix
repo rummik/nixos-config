1d4ed7c7cb1d55fe1a9d957f091b85d413423efc
Kimberly Zick (rummik) <k@9k1.us>
2018-02-07 19:02:18 -0500

    Switch home-manager to a commit ref

--- options/home-manager.nix
+++ options/home-manager.nix
@@ -1,7 +1,10 @@
 { ... }:
 
-{
-  imports = [
-    (import (builtins.fetchTarball https://github.com/rycee/home-manager/archive/nixos-module.tar.gz) {}).nixos
-  ];
-}
+let
+  ref = "1bc59f729047886b845ffd9162d40593fad5c7f0";
+in
+  {
+    imports = [
+      (import (builtins.fetchTarball "https://github.com/rycee/home-manager/archive/${ref}.tar.gz") {}).nixos
+    ];
+  }
