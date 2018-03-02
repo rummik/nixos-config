d630174d2f4df2e9a402379bdd3599a70105e79d
Kimberly Zick (rummik) <k@9k1.us>
2018-03-01 19:56:50 -0500

    More Chrome extensions, link to GH pages for each of them

--- cfgs/chromium.nix
+++ cfgs/chromium.nix
@@ -28,10 +28,13 @@
     enable = true;
 
     extensions = [
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
-      "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
-      "gcbommkclmclpchllfjekcdonpmejbdp" # HTTPS Everywhere
-      "klbibkeccnjlkjkiokjodocebajanakg" # The Great Suspender
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin - https://github.com/gorhill/uBlock
+      "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium - https://github.com/philc/vimium
+      "gcbommkclmclpchllfjekcdonpmejbdp" # HTTPS Everywhere - https://github.com/EFForg/https-everywhere
+      "klbibkeccnjlkjkiokjodocebajanakg" # The Great Suspender - https://github.com/deanoemcke/thegreatsuspender
+      "dhdgffkkebhmkfjojejmpbldmpobfkfo" # Tampermonkey - https://github.com/Tampermonkey/tampermonkey
+      "hhinaapppaileiechjoiifaancjggfjm" # Last.fm scrobbler - https://github.com/web-scrobbler/web-scrobbler
+      "mjoedlfflcchnleknnceiplgaeoegien" # Isometric contributions - https://github.com/jasonlong/isometric-contributions
     ];
 
     # Enable Chrome Cast
