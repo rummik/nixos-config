e53fd13a2f8a6014858ca6a2991a10d538a7db38
Kimberly Zick (rummik) <k@9k1.us>
2018-04-09 01:51:27 -0400

    Update WeeChat overrides to work on NixOS 18.03

--- cfgs/weechat.nix
+++ cfgs/weechat.nix
@@ -4,19 +4,22 @@
   environment.systemPackages = with pkgs; [ weechat ];
 
   nixpkgs.config.packageOverrides = pkgs: {
-    weechat = pkgs.weechat.overrideAttrs (oldAttrs: rec {
-      version = "2.1";
-      name = "weechat-${version}";
+    weechat =
+      (pkgs.weechat.override {
+        extraBuildInputs = with pkgs; [
+          pythonPackages.websocket_client
+          pythonPackages.dbus-python
+        ];
+      })
 
-      src = pkgs.fetchurl {
-        url = "http://weechat.org/files/src/weechat-${version}.tar.bz2";
-        sha256 = "0fq68wgynv2c3319gmzi0lz4ln4yrrk755y5mbrlr7fc1sx7ffd8";
-      };
+      .overrideAttrs (oldAttrs: rec {
+        version = "2.1";
+        name = "weechat-${version}";
 
-      buildInputs = with pkgs; oldAttrs.buildInputs ++ [
-        python27Packages.websocket_client
-        python27Packages.dbus-python
-      ];
-    });
+        src = pkgs.fetchurl {
+          url = "http://weechat.org/files/src/weechat-${version}.tar.bz2";
+          sha256 = "0fq68wgynv2c3319gmzi0lz4ln4yrrk755y5mbrlr7fc1sx7ffd8";
+        };
+      });
   };
 } 
