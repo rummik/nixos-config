f0a238998f2456ee0acfe1315beda8d9da65924e
Kimberly Zick (rummik) <k@9k1.us>
2018-08-06 07:17:47 -0400

    [weechat] Fix WeeChat Python plugins

--- cfgs/weechat.nix
+++ cfgs/weechat.nix
@@ -4,21 +4,16 @@
   environment.systemPackages = with pkgs; [ weechat ];
 
   nixpkgs.config.packageOverrides = pkgs: {
-    weechat =
-      (pkgs.weechat.override {
-        extraBuildInputs = with pkgs; [
-          pythonPackages.websocket_client
-          pythonPackages.dbus-python
-        ];
-      })
+    weechat = (pkgs.weechat.override {
+        configure = { availablePlugins, ... }: {
+          plugins = with availablePlugins; [
+            perl tcl ruby guile lua
 
-      .overrideAttrs (oldAttrs: rec {
-        version = "2.1";
-        name = "weechat-${version}";
-
-        src = pkgs.fetchurl {
-          url = "http://weechat.org/files/src/weechat-${version}.tar.bz2";
-          sha256 = "0fq68wgynv2c3319gmzi0lz4ln4yrrk755y5mbrlr7fc1sx7ffd8";
+            (python.withPackages (ps: with ps; [
+              websocket_client
+              dbus-python
+            ]))
+          ];
         };
       });
   };
