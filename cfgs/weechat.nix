253e86c3c9617e185356d83eda89f3f1bb14be7e
Kimberly Zick (rummik) <k@9k1.us>
2018-08-06 12:50:03 -0400

    Things

--- cfgs/weechat.nix
+++ cfgs/weechat.nix
@@ -4,7 +4,8 @@
   environment.systemPackages = with pkgs; [ weechat ];
 
   nixpkgs.config.packageOverrides = pkgs: {
-    weechat = (pkgs.weechat.override {
+    weechat =
+      (pkgs.weechat.override {
         configure = { availablePlugins, ... }: {
           plugins = with availablePlugins; [
             perl tcl ruby guile lua
@@ -16,5 +17,19 @@
           ];
         };
       });
+
+      # angrily commenting this out until the weechat package is fixed and
+      # reasonable again instead of the nonsense that got merged into master
+      /*.overrideAttrs (old: rec {
+        unwrapped = pkgs.weechat.unwrapped.overrideAttrs (oldAttrs: rec {
+          version = "2.2";
+          name = "weechat-${version}";
+
+          src = builtins.fetchTarball {
+            url = "http://weechat.org/files/src/weechat-${version}.tar.bz2";
+            sha256 = "0adsnczsi74v7bqf2xd1n6y04rq0zrpqfy2gxqwxvq857ngsii47";
+          };
+        });
+      });*/
   };
 } 
