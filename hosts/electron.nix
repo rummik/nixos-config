42b13790e349d4b3e2ae1413e243a99ad480a024
Kimberly Zick (rummik) <k@9k1.us>
2018-10-07 23:45:01 -0400

    Add a dispatcher script to disable wifi when wired networking is connected

--- hosts/electron.nix
+++ hosts/electron.nix
@@ -29,4 +29,29 @@
   environment.systemPackages = with pkgs; [
     parted
   ];
+
+  networking.networkmanager.dispatcherScripts = [{
+    type = "basic";
+
+    source = pkgs.writeText "disable-wireless-when-wired" ''
+      IFACE=$1
+      ACTION=$2
+      nmcli=${pkgs.networkmanager}/bin/nmcli
+
+      case ''${IFACE} in
+          eth*|en*)
+              case ''${ACTION} in
+                  up)
+                      logger "disabling wifi radio"
+                      $nmcli radio wifi off
+                      ;;
+                  down)
+                      logger "enabling wifi radio"
+                      $nmcli radio wifi on
+                      ;;
+              esac
+              ;;
+      esac
+    '';
+  }];
 }
