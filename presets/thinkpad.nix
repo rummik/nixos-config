b7036f2ec6ffc9ddb1434a0235a1060b7c980c8b
Kimberly Zick (rummik) <k@9k1.us>
2018-11-01 07:14:16 -0400

    [presets/thinkpad] Switch to modesetting driver, fiddle with power saving and GPU accell

--- presets/thinkpad.nix
+++ presets/thinkpad.nix
@@ -1,13 +1,35 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ehci_pci"
+    "ahci"
+    "usbhid"
+    "sd_mod"
+    "sdhci_pci"
+  ];
+
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  boot.kernelParams = [
+    "i915.enable_rc6=7"
+    "i915.enable_psr=2"
+    "i915.enable_fbc=1"
+    "i915.lvds_downclock=1"
+    "i915.semaphores=1"
+  ];
+
   nix.maxJobs = lib.mkDefault 4;
+
   powerManagement.cpuFreqGovernor = "powersave";
   powerManagement.powertop.enable = true;
+
+  hardware.opengl = {
+    extraPackages = [ pkgs.vaapiIntel ];
+    s3tcSupport = true;
+  };
 
   hardware.trackpoint = {
     enable = true;
@@ -16,8 +38,18 @@
     speed = 97;
   };
 
-  services.xserver.libinput = {
-    enable = true;
-    sendEventsMode = "disabled";
+  services.xserver = {
+    libinput = {
+      enable = true;
+      sendEventsMode = "disabled";
+    };
+
+    videoDrivers = [ "modesetting" ];
+    useGlamor = true;
+
+    deviceSection = ''
+      Option "TearFree" "true"
+      Option "AccelMethod" "sna"
+    '';
   };
 }
