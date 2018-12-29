484c52f79487dc9dbccd30e45b8b6b66abd066d1
Kimberly Zick (rummik) <k@9k1.us>
2018-12-29 18:13:03 -0500

    [cfgs/home/git] Add ignores

--- cfgs/home/git.nix
+++ cfgs/home/git.nix
@@ -5,8 +5,16 @@
     enable = true;
     userName = "*Kim Zick (rummik)";
     userEmail = "k@9k1.us";
+
     signing = {
       key = "D72F9A68A07514C3815A0D31739CF2CBEF8202E5";
     };
+
+    ignores = [
+      "*.sw?"
+      "*.todo"
+      "*.undodir"
+      "Session.vim"
+    ];
   };
 }
