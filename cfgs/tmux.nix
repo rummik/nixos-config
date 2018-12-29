b28ab4234d1a0ff00a5f66bec4fa96ebd21876f2
Kimberly Zick (rummik) <k@9k1.us>
2018-12-29 17:02:35 -0500

    [cfgs/tmux] Add some patches for resurrect and switch on some features

--- cfgs/tmux.nix
+++ cfgs/tmux.nix
@@ -19,14 +19,28 @@
     plugins = with pkgs.tmuxPlugins; [
       (resurrect.overrideAttrs (oldAttrs: rec {
         patches = [
+          # cmdline strategy
           (pkgs.fetchpatch {
-            url = "https://github.com/tmux-plugins/tmux-resurrect/commit/edd8132befb336b71190f55498dccc1772a8a893.patch";
-            sha256 = "0px48gn5ja4z8mkhlm6b545r72rrqjp8wqnf642kh3r4walahy7j";
+            url = "https://patch-diff.githubusercontent.com/raw/tmux-plugins/tmux-resurrect/pull/283.patch";
+            sha256 = "05d1a5642rx8b2b0fxy55sjha8hf4ik6bwdl754hzawayh7jcy9d";
           })
 
+          # mosh-client strategy
           (pkgs.fetchpatch {
-            url = "https://github.com/tmux-plugins/tmux-resurrect/commit/55536f8685d7fe96459fdd09a29a6fb01f0c8a80.patch";
-            sha256 = "13sq0xa3z2msq0ldvff3qdfn217g7c2vc90py45vb8lg91p5r735";
+            url = "https://patch-diff.githubusercontent.com/raw/tmux-plugins/tmux-resurrect/pull/284.patch";
+            sha256 = "1byhbi53dn1zglymva1h98zfw0m57nfravbv6pgppl7vvyw6f0v4";
+          })
+
+          # zsh history save/restore fixes
+          (pkgs.fetchpatch {
+            url = "https://patch-diff.githubusercontent.com/raw/tmux-plugins/tmux-resurrect/pull/285.patch";
+            sha256 = "1bdvr7vj459f0kwvgvmsh1br06wk2w5hr1zjc2jfhab6mw1wb084";
+          })
+
+          # basename match strategy
+          (pkgs.fetchpatch {
+            url = "https://patch-diff.githubusercontent.com/raw/tmux-plugins/tmux-resurrect/pull/286.patch";
+            sha256 = "033x10g8agkm5vswiyg0mjqswbq1n07927b0py6q9rc2g8r95k08";
           })
         ];
       }))
@@ -70,10 +84,11 @@
 
       # resurrect options
       set -g @resurrect-capture-pane-contents 'on'
-      set -g @resurrect-processes 'mosh ssh ~nvim "~yarn watch"'
+      set -g @resurrect-processes 'mosh-client man "~yarn watch"'
       set -g @resurrect-save-command-strategy 'cmdline'
+      set -g @resurrect-process-match-strategy 'basename'
       #set -g @resurrect-strategy-nvim 'session'
-      #set -g @resurrect-save-shell-history 'off'
+      #set -g @resurrect-save-shell-history 'on'
 
       # continuum options
       set -g @continuum-save-interval '15'
