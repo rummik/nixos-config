a0177294d8d06adaf552bcdf522d3cc7bfd5f1d5
Kimberly Zick (rummik) <k@9k1.us>
2019-06-12 15:39:39 -0400

    [profiles/users] User profile fixings

--- profiles/users.nix
+++ profiles/users.nix
@@ -1,48 +1,50 @@
 { pkgs, lib, isLinux, isDarwin, ... }:
 
 let
-  inherit (lib) optionalAttrs;
+  inherit (lib) mkIf mkMerge optionalAttrs;
 
   authorizedKeys = import ./authorized-keys.nix;
   homeManager = import ../config/home-manager.nix;
 in
-  {
-    home-manager.users.rummik = homeManager;
+  mkMerge [
+    {
+      home-manager.users.rummik = homeManager;
 
-    users.users.rummik =
-      {
-        name = "*Kim Zick";
-      }
+      users.users.rummik =
+        {
+          isNormalUser = true;
+        }
 
-      // optionalAttrs isLinux {
-        uid = 1000;
-        isNormalUser = true;
-        linger = true;
+        // optionalAttrs isLinux {
+          uid = 1000;
+          linger = true;
 
-        # should really choose something hashed, but XKCD-936 amuses me
-        initialPassword = "correct horse battery staple";
+          # should really choose something hashed, but XKCD-936 amuses me
+          initialPassword = "correct horse battery staple";
 
-        openssh.authorizedKeys.keys = authorizedKeys;
+          openssh.authorizedKeys.keys = authorizedKeys;
 
-        extraGroups = [
-          "adbusers"
-          "audio"
-          "dialout" # ttyACM*
-          "docker"
-          "networkmanager"
-          "render"
-          "scanner" "lp" # SANE
-          "vboxusers"
-          "video"
-          "wheel"
-          "wireshark"
-        ];
-      };
-  }
+          extraGroups = [
+            "adbusers"
+            "audio"
+            "dialout" # ttyACM*
+            "docker"
+            "networkmanager"
+            "render"
+            "scanner" "lp" # SANE
+            "vboxusers"
+            "video"
+            "wheel"
+            "wireshark"
+          ];
+        };
+    }
 
-  // optionalAttrs isLinux {
-    home-manager.users.root = homeManager;
+    (mkIf isLinux {
+      users.defaultUserShell = pkgs.zsh;
 
-    users.defaultUserShell = pkgs.zsh;
-    users.users.root.openssh.authorizedKeys.keys = authorizedKeys;
-  }
+      home-manager.users.root = homeManager;
+
+      users.users.root.openssh.authorizedKeys.keys = authorizedKeys;
+    })
+  ]
