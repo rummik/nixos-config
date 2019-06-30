5d2cfd104876559c0573e0b2a5b39316d7ff336c
Kimberly Zick (rummik) <k@9k1.us>
2019-06-30 15:28:04 -0400

    [config/home-manager/obs-studio] Disable OBS Studio for root

--- config/home-manager/obs-studio.nix
+++ config/home-manager/obs-studio.nix
@@ -1,10 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+
+let
+
+  inherit (config.home) username;
+
+in
 
 {
-  programs.obs-studio = {
-    enable = true;
-    plugins = [
-      pkgs.obs-linuxbrowser
+  programs.obs-studio =  {
+    enable = username != "root";
+    plugins = with pkgs; [
+      obs-linuxbrowser
     ];
   };
 }
