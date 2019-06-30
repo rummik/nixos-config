91cebf290c005d8dc00f54aaa49f494a292aea70
Kimberly Zick (rummik) <k@9k1.us>
2019-06-30 17:20:53 -0400

    [overlays] Rework default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,16 +1,20 @@
 self: super:
 
 let
+
+  inherit (import ../channels) __nixPath;
   inherit (builtins) mapAttrs readDir;
   inherit (super) callPackage;
   inherit (super.lib) optionalAttrs;
   inherit (super.stdenv) isLinux isDarwin;
 
-  callPackages = path: ignore:
+  callPackages = path:
     mapAttrs
-      (name: value: callPackage (path + "/${name}") { ft = import ./ft.nix super.lib; })
-      (removeAttrs (readDir path) ignore);
+      (name: value: callPackage (path + "/${name}") { ft = import <ft>; })
+      (removeAttrs (readDir path) [ "os-specific" "default.nix" ]);
+
 in
-  callPackages ./. [ "os-specific" "default.nix" "ft.nix" ]
-  // optionalAttrs isLinux (callPackages ./os-specific/linux [])
-  // optionalAttrs isDarwin (callPackages ./os-specific/darwin [])
+
+callPackages ./.
+// optionalAttrs isLinux (callPackages ./os-specific/linux)
+// optionalAttrs isDarwin (callPackages ./os-specific/darwin)
