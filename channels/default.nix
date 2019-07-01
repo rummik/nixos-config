eafc290e44a5f34b8d0b5564e2f26e1b578806f6
*Kim Zick (rummik) <kim@accelaschool.com>
2019-06-30 23:12:57 -0400

    [channels] Fix macOS compat, add binary-caches

--- channels/default.nix
+++ channels/default.nix
@@ -1,13 +1,15 @@
 let
 
   __nixPath = [
+    { prefix = "binary-caches"; path = ./binary-caches; }
     { prefix = "darwin"; path = ./darwin; }
+    { prefix = "darwin-config"; path = ../configuration.nix; }
     { prefix = "ft"; path = ../lib/ft.nix; }
     { prefix = "home-manager"; path = ./home-manager; }
     { prefix = "nixos"; path = ./nixos; }
     { prefix = "nixos-config"; path = ../configuration.nix; }
-    { prefix = "nixpkgs"; path = ./nixos; }
-    { prefix = "nixpkgs-overlays"; path = ../overlays; }
+    { prefix = "nixpkgs"; path = ./nixpkgs; }
+    { prefix = "nixpkgs-overlays"; path = ../overlays/default.nix; }
   ];
 
 in
