0274695b96867b3a658c523bffb99ec7e6d1a1d7
Kimberly Zick (rummik) <k@9k1.us>
2019-07-04 17:50:31 -0400

    [channels] Update channels, include nixpkgs-unstable

--- channels/default.nix
+++ channels/default.nix
@@ -1,5 +1,4 @@
-let
-
+rec {
   __nixPath = [
     { prefix = "binary-caches"; path = ./binary-caches; }
     { prefix = "darwin"; path = ./darwin; }
@@ -10,11 +9,8 @@ let
     { prefix = "nixos-config"; path = ../configuration.nix; }
     { prefix = "nixpkgs"; path = ./nixpkgs; }
     { prefix = "nixpkgs-overlays"; path = ../overlays/default.nix; }
+    { prefix = "nixpkgs-unstable"; path = ./nixpkgs-unstable; }
   ];
 
-in
-
-{
-  inherit __nixPath;
   nixPath = map ({ prefix, path }: "${prefix}=${toString path}") __nixPath;
 }
