0eee32effab1912990eb31a0425ffab3d36c0c0a
Kimberly Zick (rummik) <k@9k1.us>
2019-07-04 17:52:44 -0400

    [overlays] Pull in Alacritty from unstable

--- overlays/common/default.nix
+++ overlays/common/default.nix
@@ -10,6 +10,12 @@ let
 in
 
 {
+  alacritty = super.callPackage <nixpkgs-unstable/pkgs/applications/misc/alacritty> {
+    inherit (super.xorg) libXcursor libXxf86vm libXi;
+    inherit (super.darwin) cf-private;
+    inherit (super.darwin.apple_sdk.frameworks) AppKit CoreFoundation CoreGraphics CoreServices CoreText Foundation OpenGL;
+  };
+
   bitlbee-discord = callPackage ./bitlbee-discord;
   bitlbee-mastodon = callPackage ./bitlbee-mastodon;
   ddpt = callPackage ./ddpt;
