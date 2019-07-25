ccf54343336ff988a79eafe78cb675372b6d1f44
*Kim Zick (rummik) <k@9k1.us>
2019-07-25 01:50:02 -0400

    Import changes from updated alacritty default config

--- config/home-manager/alacritty.nix
+++ config/home-manager/alacritty.nix
@@ -445,82 +445,68 @@ in
 
       # Mouse bindings
       #
-      # Available fields:
-      #   - mouse
-      #   - action
-      #   - mods (optional)
+      # Mouse bindings are specified as a list of objects, much like the key
+      # bindings further below.
       #
-      # Values for `mouse`:
+      # Each mouse binding will specify a:
+      #
+      # - `mouse`:
+      #
       #   - Middle
       #   - Left
       #   - Right
       #   - Numeric identifier such as `5`
       #
-      # All available `mods` and `action` values are documented in the key binding
-      # section.
+      # - `action` (see key bindings)
+      #
+      # And optionally:
+      #
+      # - `mods` (see key bindings)
       #mouse_bindings = [
       #  { mouse = "Middle"; action = "PasteSelection"; }
       #];
 
       # Key bindings
       #
-      # Key bindings are specified as a list of objects. Each binding will specify a
-      # key and modifiers required to trigger it, terminal modes where the binding is
-      # applicable, and what should be done when the key binding fires. It can either
-      # send a byte sequence to the running application (`chars`), execute a
-      # predefined action (`action`) or fork and execute a specified command plus
-      # arguments (`command`).
+      # Key bindings are specified as a list of objects. For example, this is the
+      # default paste binding:
       #
-      # Bindings are always filled by default, but will be replaced when a new binding
-      # with the same triggers is defined. To unset a default binding, it can be
-      # mapped to the `None` action.
-      #
-      # Example:
       #   `{ key = "V"; mods = "Control|Shift"; action = "Paste"; }`
       #
-      # Available fields:
-      #   - key
-      #   - mods (optional)
-      #   - chars | action | command (exactly one required)
-      #   - mode (optional)
+      # Each key binding will specify a:
       #
-      # Values for `key`:
-      #   - `A` -> `Z`
-      #   - `F1` -> `F12`
-      #   - `Key1` -> `Key0`
+      # - `key`: Identifier of the key pressed
       #
-      #   A full list with available key codes can be found here:
-      #   https://docs.rs/glutin/*/glutin/enum.VirtualKeyCode.html#variants
+      #    - A-Z
+      #    - F1-F12
+      #    - Key0-Key9
       #
-      #   Instead of using the name of the keys, the `key` field also supports using
-      #   the scancode of the desired key. Scancodes have to be specified as a
-      #   decimal number.
-      #   This command will allow you to display the hex scancodes for certain keys:
-      #     `showkey --scancodes`
+      #    A full list with available key codes can be found here:
+      #    https://docs.rs/glutin/*/glutin/enum.VirtualKeyCode.html#variants
       #
-      # Values for `mods`:
-      #   - Command
-      #   - Control
-      #   - Super
-      #   - Shift
-      #   - Alt
+      #    Instead of using the name of the keys, the `key` field also supports using
+      #    the scancode of the desired key. Scancodes have to be specified as a
+      #    decimal number. This command will allow you to display the hex scancodes
+      #    for certain keys:
       #
-      #   Multiple `mods` can be combined using `|` like this: `mods: Control|Shift`.
-      #   Whitespace and capitalization is relevant and must match the example.
+      #       `showkey --scancodes`.
       #
-      # Values for `chars`:
-      #   The `chars` field writes the specified string to the terminal. This makes
-      #   it possible to pass escape sequences.
-      #   To find escape codes for bindings like `PageUp` ("\x1b[5~"), you can run
-      #   the command `showkey -a` outside of tmux.
-      #   Note that applications use terminfo to map escape sequences back to
-      #   keys. It is therefore required to update the terminfo when
-      #   changing an escape sequence.
+      # Then exactly one of:
       #
-      # Values for `action`:
+      # - `chars`: Send a byte sequence to the running application
+      #
+      #    The `chars` field writes the specified string to the terminal. This makes
+      #    it possible to pass escape sequences. To find escape codes for bindings
+      #    like `PageUp` (`"\x1b[5~"`), you can run the command `showkey -a` outside
+      #    of tmux. Note that applications use terminfo to map escape sequences back
+      #    to keys. It is therefore required to update the terminfo when changing an
+      #    escape sequence.
+      #
+      # - `action`: Execute a predefined action
+      #
+      #   - Copy
       #   - Paste
       #   - PasteSelection
-      #   - Copy
       #   - IncreaseFontSize
       #   - DecreaseFontSize
       #   - ResetFontSize
@@ -533,22 +519,50 @@ in
       #   - ClearHistory
       #   - Hide
       #   - Quit
-      #   - ClearLogNotice
+      #   - ToggleFullscreen
       #   - SpawnNewInstance
+      #   - ClearLogNotice
       #   - None
       #
-      # Values for `command`:
-      #   The `command` field must be a map containing a `program` string and
-      #   an `args` array of command line parameter strings.
+      #   (macOS only):
+      #   - ToggleSimpleFullscreen: Enters fullscreen without occupying another space
       #
-      #   Example:
-      #       `command = { program = "alacritty"; args = [ "-e", "vttest" ]; };`
+      # - `command`: Fork and execute a specified command plus arguments
       #
-      # Values for `mode`:
-      #   - ~AppCursor
-      #   - AppCursor
-      #   - ~AppKeypad
-      #   - AppKeypad
+      #    The `command` field must be a map containing a `program` string and an
+      #    `args` array of command line parameter strings. For example:
+      #       `{ program: "alacritty", args: ["-e", "vttest"] }`
+      #
+      # And optionally:
+      #
+      # - `mods`: Key modifiers to filter binding actions
+      #
+      #    - Command
+      #    - Control
+      #    - Option
+      #    - Super
+      #    - Shift
+      #    - Alt
+      #
+      #    Multiple `mods` can be combined using `|` like this:
+      #       `mods: Control|Shift`.
+      #    Whitespace and capitalization are relevant and must match the example.
+      #
+      # - `mode`: Indicate a binding for only specific terminal reported modes
+      #
+      #    This is mainly used to send applications the correct escape sequences
+      #    when in different modes.
+      #
+      #    - AppCursor
+      #    - AppKeypad
+      #    - Alt
+      #
+      #    A `~` operator can be used before a mode to apply the binding whenever
+      #    the mode is *not* active, e.g. `~Alt`.
+      #
+      # Bindings are always filled by default, but will be replaced when a new
+      # binding with the same triggers is defined. To unset a default binding, it can
+      # be mapped to the `None` action.
       key_bindings = flatten [
         # (Windows/Linux only)
         #(optionals (isLinux || isWindows) [
@@ -599,7 +613,7 @@ in
           { key = "Add";      mods = "Control";         action = "IncreaseFontSize";  }
           { key = "Subtract"; mods = "Control";         action = "DecreaseFontSize";  }
           { key = "Minus";    mods = "Control";         action = "DecreaseFontSize";  }
-          { key = "Return";   mods = "Alt";             action = "ToggleFullScreen";  }
+          #{ key = "Return";   mods = "Alt";             action = "ToggleFullScreen";  }
         ])
 
         { key = "N";        mods = "Control|Shift";   action = "SpawnNewInstance";                     }
