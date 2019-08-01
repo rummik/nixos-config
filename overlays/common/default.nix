372fc57f72825d0c603d82dc1f50bbf121d1b408
Kimberly Zick (rummik) <k@9k1.us>
2019-08-01 09:59:14 -0400

    Update alacrity override

--- overlays/common/default.nix
+++ overlays/common/default.nix
@@ -12,8 +12,7 @@ in
 
   alacritty = callPackage <nixpkgs-unstable/pkgs/applications/misc/alacritty> {
     inherit (super.xorg) libXcursor libXxf86vm libXi;
-    inherit (super.darwin) cf-private;
-    inherit (super.darwin.apple_sdk.frameworks) AppKit CoreFoundation CoreGraphics CoreServices CoreText Foundation OpenGL;
+    inherit (super.darwin.apple_sdk.frameworks) AppKit CoreGraphics CoreServices CoreText Foundation OpenGL;
   };
 
   bitlbee-discord = callPackage ./bitlbee-discord {};
