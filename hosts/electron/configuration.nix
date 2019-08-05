dbbf143e9f4fc8bbd1d257e13de2d34517ceb219
Kimberly Zick (rummik) <k@9k1.us>
2019-08-05 12:41:46 -0400

    Enable disable-wireless-when-wired dispatcher again

--- hosts/electron/configuration.nix
+++ hosts/electron/configuration.nix
@@ -42,28 +42,28 @@
     passwordAuthentication = false;
   };
 
-#  networking.networkmanager.dispatcherScripts = [{
-#    type = "basic";
-#
-#    source = pkgs.writeText "disable-wireless-when-wired" /* sh */ ''
-#      IFACE=$1
-#      ACTION=$2
-#      nmcli=${pkgs.networkmanager}/bin/nmcli
-#
-#      case ''${IFACE} in
-#          eth*|en*)
-#              case ''${ACTION} in
-#                  up)
-#                      logger "disabling wifi radio"
-#                      $nmcli radio wifi off
-#                      ;;
-#                  down)
-#                      logger "enabling wifi radio"
-#                      $nmcli radio wifi on
-#                      ;;
-#              esac
-#              ;;
-#      esac
-#    '';
-#  }];
+  networking.networkmanager.dispatcherScripts = [{
+    type = "basic";
+
+    source = pkgs.writeText "disable-wireless-when-wired" /* sh */ ''
+      IFACE=$1
+      ACTION=$2
+      nmcli=${pkgs.networkmanager}/bin/nmcli
+
+      case ''${IFACE} in
+          eth*|en*)
+              case ''${ACTION} in
+                  up)
+                      logger "disabling wifi radio"
+                      $nmcli radio wifi off
+                      ;;
+                  down)
+                      logger "enabling wifi radio"
+                      $nmcli radio wifi on
+                      ;;
+              esac
+              ;;
+      esac
+    '';
+  }];
 }
