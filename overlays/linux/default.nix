dee629708c09a5201ee9e9da7b0e075dd11d6a49
*Kim Zick (rummik) <k@9k1.us>
2019-09-30 01:12:48 -0400

    Pull lutris in from unstable

--- overlays/linux/default.nix
+++ overlays/linux/default.nix
@@ -12,20 +12,6 @@ in
 optionalAttrs isLinux rec {
   firmwareLinuxNonfree = callPackage <nixpkgs-unstable/pkgs/os-specific/linux/firmware/firmware-linux-nonfree> {};
 
-  libstrangle = callPackage <nixpkgs-unstable/pkgs/tools/X11/libstrangle> {
-    stdenv = super.stdenv_32bit;
-  };
-
-  dumb = callPackage <nixpkgs-unstable/pkgs/misc/dumb> { };
-  sndio = callPackage <nixpkgs-unstable/pkgs/misc/sndio> { };
-
-  lutris = callPackage <nixpkgs-unstable/pkgs/applications/misc/lutris/chrootenv.nix> { };
-
-  lutris-unwrapped = python3.pkgs.callPackage <nixpkgs-unstable/pkgs/applications/misc/lutris>  {
-    inherit (gnome3) gnome-desktop libgnome-keyring webkitgtk;
-    wine = wineWowPackages.staging;
-    gdk-pixbuf = callPackage <nixpkgs-unstable/pkgs/development/libraries/gdk-pixbuf> { };
-  };
 
   mailspring = callPackage ./mailspring {};
   minipro = callPackage ./minipro {};
--- profiles/games.nix
+++ profiles/games.nix
@@ -1,12 +1,19 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
+
 {
   imports = [
     ../config/steam.nix
   ];
 
-  environment.systemPackages = with pkgs; [
-    minetest
-    multimc
-    lutris
-  ];
+  environment.systemPackages =
+    (with pkgs; [
+      minetest
+      multimc
+    ])
+
+    ++
+
+    (with pkgs-unstable; [
+      lutris
+    ]);
 }
