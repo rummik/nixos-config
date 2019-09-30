fa442f027cac8004611b8c314e808efb9ae3af73
*Kim Zick (rummik) <k@9k1.us>
2019-09-30 01:07:46 -0400

    Add kontact config

---
+++ config/kontact.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = 
+    (with pkgs.kdeApplications; [
+      akonadi
+      akonadi-calendar
+      akonadi-contacts
+      akonadi-import-wizard
+      akonadi-mime
+      akonadi-notes
+      akonadi-search
+      akonadiconsole
+      akregator
+      kaddressbook
+      kmail
+      kmail-account-wizard
+      kmailtransport
+      kontact
+      kontactinterface
+      korganizer
+    ]);
+}
