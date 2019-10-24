a5e89092d2ad26924beb6bbba68b5be42842c028
*Kim Zick (rummik) <k@9k1.us>
2019-10-24 12:52:12 -0400

    Enable powerline in neovim config

--- config/neovim.nix
+++ config/neovim.nix
@@ -62,6 +62,7 @@
           let g:airline_section_x=""
           let g:airline_section_y=""
           let g:airline_skip_empty_sections = 1
+          let g:airline_powerline_fonts = 1
 
           " General
           filetype plugin indent on
