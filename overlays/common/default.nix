ccb951352ea9cf64d4fec98c001181eea7385e29
*Kim Zick (rummik) <k@9k1.us>
2020-01-02 11:11:14 -0500

    [overlays/common] Add missing ZSH deps

--- overlays/common/default.nix
+++ overlays/common/default.nix
@@ -30,21 +30,70 @@ rec {
   };
 
   zshPlugins = (import ./zsh-plugins) {
-    inherit (super) lib fetchFromGitHub fetchFromGitLab;
+    inherit (super) stdenv lib writeTextFile fetchFromGitHub fetchFromGitLab
+      nix-zsh-completions;
   };
 
-  kubernetes-helm3 = super.kubernetes-helm.overrideAttrs (helm2: rec {
-    version = "3.0.0-beta.4";
+  revolver = mkPackage {} (
+    { stdenv, fetchFromGitHub, writeScript, zsh }:
 
-    src = fetchFromGitHub {
-      owner = "helm";
-      repo = "helm";
-      rev = "v${version}";
-      sha256 = "18ly31db2kxybjlisz8dfz3cdxs7j2wsh4rx5lwhbm5hpp42h17d";
-    };
+    stdenv.mkDerivation rec {
+      name = "revolver";
+      version = "0.2.3";
 
-    buildFlagsArray = replaceStrings [ helm2.version ] [ version ] helm2.buildFlagsArray;
-  });
+      src = fetchFromGitHub {
+        owner = "molovo";
+        repo = "revolver";
+        rev = "v${version}";
+        sha256 = "122zb5vpsaqc2s8dm4i79y7vdayn8prpv9jfyypzp7v38hq2h9h0";
+      };
+
+      dontStrip = true;
+      dontPatchELF = true;
+      dontBuild = true;
+
+      buildInputs = [ zsh ];
+
+      installPhase = /* sh */ ''
+        mkdir -p $out/{bin,share/zsh/site-functions}
+        chmod a+x revolver
+        cp revolver $out/bin
+        cp revolver.zsh-completion $out/share/zsh/site-functions/_revolver
+      '';
+    }
+  );
+
+  zunit = mkPackage { inherit revolver; } (
+    { stdenv, fetchFromGitHub, writeScript, zsh, revolver }:
+
+    stdenv.mkDerivation rec {
+      name = "zunit";
+      version = "0.8.2";
+
+      src = fetchFromGitHub {
+        owner = "zunit-zsh";
+        repo = "zunit";
+        rev = "v${version}";
+        sha256 = "0dxa0fmjgqrf5awx7mh9s09pqdmw0nzfpnhbhs0apjx1i7k1nm96";
+      };
+
+      dontStrip = true;
+      dontPatchELF = true;
+
+      buildInputs = [ zsh revolver ];
+
+      buildPhase = /* sh */ ''
+        [[ -f build.zsh ]] && zsh build.zsh
+      '';
+
+      installPhase = /* sh */ ''
+        mkdir -p $out/{bin,share/zsh/site-functions}
+        chmod a+x zunit
+        cp zunit $out/bin
+        cp zunit.zsh-completion $out/share/zsh/site-functions/_zunit
+      '';
+    }
+  );
 
   proxmark3 = super.proxmark3.overrideAttrs (pm3: {
     src = super.fetchgit rec {
