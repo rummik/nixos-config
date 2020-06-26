b5374716eb1e1e168e543aa5dd04d88c9011e041
*Kim Zick (rummik) <k@9k1.us>
2020-06-26 10:30:12 -0400

    [profiles/workstation] Update workstation config

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -1,13 +1,14 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 
 {
   imports = [
     ./plasma5.nix
     ../config/barrier.nix
+    ../config/docker.nix
     ../config/keybase.nix
     ../config/networkmanager.nix
     ../config/nodejs.nix
-    ../config/vlc.nix
+    #../config/vlc.nix
     ../config/watson.nix
     ../config/yubikey.nix
   ];
@@ -19,6 +20,7 @@
     minikube
     minipro
     nixops
+    morph
     tio
     xclip
     xsel
@@ -38,9 +40,12 @@
   programs.adb.enable = true;
 
   services.udev.extraRules = /* udevrules */ ''
-    ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_interface", \
-      ATTRS{idVendor}=="0403", ATTRS{idProduct}=="a6d0", \
-      DRIVER=="", RUN+="/sbin/modprobe -b ftdi_sio"
+    ACTION=="add", SUBSYSTEM=="usb", \
+      ENV{DEVTYPE}=="usb_interface", \
+      ATTRS{idVendor}=="0403", \
+      ATTRS{idProduct}=="a6d0", \
+      DRIVER=="", \
+      RUN+="/sbin/modprobe -b ftdi_sio"
 
     ACTION=="add", SUBSYSTEM=="drivers", \
       ENV{DEVPATH}=="/bus/usb-serial/drivers/ftdi_sio", \
@@ -55,5 +60,24 @@
       ATTRS{interface}=="BeagleBone", \
       ATTRS{bInterfaceNumber}=="01", \
       SYMLINK+="beaglebone-serial"
+
+    ACTION=="add", SUBSYSTEM=="tty", \
+      ATTRS{idVendor}=="10c4", \
+      ATTRS{idProduct}=="8a2a", \
+      ENV{ID_USB_INTERFACE_NUM}=="00", \
+      GROUP="100", \
+      RUN+="${pkgs.coreutils}/bin/ln -sf %N /dev/iot/zwave", \
+      SYMLINK+="iot/zwave"
+
+    ACTION=="add", SUBSYSTEM=="tty", \
+      ATTRS{idVendor}=="10c4", \
+      ATTRS{idProduct}=="8a2a", \
+      ENV{ID_USB_INTERFACE_NUM}=="01", \
+      GROUP="100", \
+      RUN+="${pkgs.coreutils}/bin/ln -sf %N /dev/iot/zigbee", \
+      SYMLINK+="iot/zigbee"
+
+    # Longan Nano
+    ATTRS{idVendor}=="28e9", ATTRS{idProduct}=="0189", MODE="0666"
   '';
 }
