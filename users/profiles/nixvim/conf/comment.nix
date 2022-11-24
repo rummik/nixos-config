4bc1ea05b21a4682d63c24a481ebf6a6c5973044
*Kim Zick (rummik) <k@9k1.us>
2022-11-24 14:57:36 -0500

    fix(vimnix): update comment-nvim hooks to match new version

--- users/profiles/nixvim/conf/comment.nix
+++ users/profiles/nixvim/conf/comment.nix
@@ -2,7 +2,7 @@
   programs.nixvim = {
     plugins.comment-nvim.enable = true;
     # Neovim seems to register <C-/> as <C-_>
-    maps.normal."<c-_>" = "<Plug>(comment_toggle_current_linewise)";
+    maps.normal."<c-_>" = "<Plug>(comment_toggle_linewise_current)";
     maps.visual."<c-_>" = "<Plug>(comment_toggle_linewise_visual)";
   };
 }
