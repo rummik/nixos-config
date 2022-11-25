557d3e67501d25b369c19f00022939379d6551c7
*Kim Zick (rummik) <k@9k1.us>
2022-11-24 23:00:20 -0500

    host(muon): remove extra tlp options from host

--- hosts/nixos/muon.nix
+++ hosts/nixos/muon.nix
@@ -55,13 +55,6 @@
     8123
   ];
 
-  services.tlp.settings = {
-    #CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-    #CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    CPU_BOOST_ON_AC = 0;
-    CPU_BOOST_ON_BAT = 0;
-  };
-
   hardware = {
     firmware = with pkgs; [
       firmwareLinuxNonfree
