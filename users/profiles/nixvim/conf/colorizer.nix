f747ae9744633ade9b195dc54352169cc73d5d6b
*Kim Zick (rummik) <k@9k1.us>
2022-11-28 23:10:05 -0500

    nixvim: update colorizer config

--- users/profiles/nixvim/conf/colorizer.nix
+++ users/profiles/nixvim/conf/colorizer.nix
@@ -1,6 +1,30 @@
 {pkgs, ...}: {
   programs.nixvim = {
-    extraConfigLua = "require 'colorizer'.setup()";
     extraPlugins = [pkgs.vimPlugins.nvim-colorizer-lua];
+    extraConfigLua = ''
+      require 'colorizer'.setup {
+        filetypes = {
+          '*';
+          css = { css = true; };
+          scss = { css = true; };
+          sass = { css = true; };
+          less = { css = true; };
+          stylus = { css = true; };
+        };
+
+        user_default_options = {
+          RGB = false;
+          names = false;
+          RRGGBBAA = true;
+          AARRGGBB = true;
+        };
+
+        buftypes = {
+          '*';
+          '!prompt';
+          '!popup';
+        };
+      }
+    '';
   };
 }
