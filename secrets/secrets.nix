d66249383b57e702bb5627b9474638be9b1842d4
*Kim Zick (rummik) <k@9k1.us>
2023-07-29 14:46:20 -0400

    agenix,git-crypt: don't double-encrypt

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -10,17 +10,19 @@ let
   allKeys = hosts ++ users;
 in {
   "secret.age".publicKeys = allKeys;
+  "maxine.age".publicKeys = [ photon rummik ];
   "rummik.age".publicKeys = hosts ++ [ rummik ];
   "rummik-grub.age".publicKeys = hosts ++ [ rummik ];
   "root.age".publicKeys = hosts ++ [ rummik ];
-  "pv-photon.key.bin.age".publicKeys = hosts ++ [ rummik ];
-  "pv-photon.password.bin.age".publicKeys = hosts ++ [ rummik ];
-  "initrd/ssh_host_ed25519_key.age".publicKeys = hosts ++ [ rummik ];
-  "initrd/ssh_host_ed25519_key.pub.age".publicKeys = hosts ++ [ rummik ];
-  "initrd/ssh_host_rsa_key.age".publicKeys = hosts ++ [ rummik ];
-  "initrd/ssh_host_rsa_key.pub.age".publicKeys = hosts ++ [ rummik ];
-  "photon/ssh_host_ed25519_key.age".publicKeys = hosts ++ [ rummik ];
-  "photon/ssh_host_ed25519_key.pub.age".publicKeys = hosts ++ [ rummik ];
-  "photon/ssh_host_rsa_key.age".publicKeys = hosts ++ [ rummik ];
-  "photon/ssh_host_rsa_key.pub.age".publicKeys = hosts ++ [ rummik ];
+  "pv-photon.key.bin.age".publicKeys = [ photon rummik ];
+  "pv-photon.password.bin.age".publicKeys = [ photon rummik ];
+  "photon/cache-priv-key.pem.age".publicKeys = [ photon rummik ];
+  "initrd/ssh_host_ed25519_key.age".publicKeys = [ photon rummik ];
+  "initrd/ssh_host_ed25519_key.pub.age".publicKeys = [ photon rummik ];
+  "initrd/ssh_host_rsa_key.age".publicKeys = [ photon rummik ];
+  "initrd/ssh_host_rsa_key.pub.age".publicKeys = [ photon rummik ];
+  "photon/ssh_host_ed25519_key.age".publicKeys = [ photon rummik ];
+  "photon/ssh_host_ed25519_key.pub.age".publicKeys = [ photon rummik ];
+  "photon/ssh_host_rsa_key.age".publicKeys = [ photon rummik ];
+  "photon/ssh_host_rsa_key.pub.age".publicKeys = [ photon rummik ];
 }
