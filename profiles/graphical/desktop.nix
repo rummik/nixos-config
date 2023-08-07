d9e415d79d50f481c3ccc29520e4d2a0622d0f18
*Kim Zick (rummik) <k@9k1.us>
2023-08-07 07:12:31 -0400

    profiles/graphical/desktop: update sound configuration

--- profiles/graphical/desktop.nix
+++ profiles/graphical/desktop.nix
@@ -99,13 +99,15 @@ in {
     '';
   };
 
+  security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
     wireplumber.enable = true;
   };
 
